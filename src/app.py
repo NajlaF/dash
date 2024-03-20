ab3848da64bee8bca8444bc0da5d2c3fce5a7d9a
NajlaF <134140635+NajlaF@users.noreply.github.com>
2024-03-20 09:48:01 +0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,142 +1,58 @@
-'''
- # Dash port of Shiny iris k-means example:
- # https://shiny.rstudio.com/gallery/kmeans-example.html
- # Source: https://dash-bootstrap-components.opensource.faculty.ai/examples/iris/
- # @ Create Time: 2024-03-20 09:19:26.912333
- # Run this app with `python app.py` and
- # visit http://127.0.0.1:8050/ in your web browser.
-'''
-import dash
-import dash_bootstrap_components as dbc
-import pandas as pd
-import plotly.graph_objs as go
-from dash import Input, Output, dcc, html
-from sklearn import datasets
-from sklearn.cluster import KMeans
+import plotly.graph_objects as go
+import plotly.figure_factory as ff
 
+# Add table data
+table_data = [['Team', 'Wins', 'Losses', 'Ties'],
+              ['Montréal<br>Canadiens', 18, 4, 0],
+              ['Dallas Stars', 18, 5, 0],
+              ['NY Rangers', 16, 5, 0],
+              ['Boston<br>Bruins', 13, 8, 0],
+              ['Chicago<br>Blackhawks', 13, 8, 0],
+              ['Ottawa<br>Senators', 12, 5, 0]]
 
-iris_raw = datasets.load_iris()
-iris = pd.DataFrame(iris_raw["data"], columns=iris_raw["feature_names"])
+# Initialize a figure with ff.create_table(table_data)
+fig = ff.create_table(table_data, height_constant=60)
 
-app = dash.Dash(
-    title="dash2",
-    external_stylesheets=[dbc.themes.BOOTSTRAP],
-    )
+# Add graph data
+teams = ['Montréal Canadiens', 'Dallas Stars', 'NY Rangers',
+         'Boston Bruins', 'Chicago Blackhawks', 'Ottawa Senators']
+GFPG = [3.54, 3.48, 3.0, 3.27, 2.83, 3.18]
+GAPG = [2.17, 2.57, 2.0, 2.91, 2.57, 2.77]
 
-# Declare server for Heroku deployment. Needed for Procfile.
-server = app.server
+# Make traces for graph
+trace1 = go.Bar(x=teams, y=GFPG, xaxis='x2', yaxis='y2',
+                marker=dict(color='#0099ff'),
+                name='Goals For<br>Per Game')
+trace2 = go.Bar(x=teams, y=GAPG, xaxis='x2', yaxis='y2',
+                marker=dict(color='#404040'),
+                name='Goals Against<br>Per Game')
 
-controls = dbc.Card(
-    [
-        html.Div(
-            [
-                dbc.Label("X variable"),
-                dcc.Dropdown(
-                    id="x-variable",
-                    options=[
-                        {"label": col, "value": col} for col in iris.columns
-                    ],
-                    value="sepal length (cm)",
-                ),
-            ]
-        ),
-        html.Div(
-            [
-                dbc.Label("Y variable"),
-                dcc.Dropdown(
-                    id="y-variable",
-                    options=[
-                        {"label": col, "value": col} for col in iris.columns
-                    ],
-                    value="sepal width (cm)",
-                ),
-            ]
-        ),
-        html.Div(
-            [
-                dbc.Label("Cluster count"),
-                dbc.Input(id="cluster-count", type="number", value=3),
-            ]
-        ),
-    ],
-    body=True,
-)
+# Add trace data to figure
+fig.add_traces([trace1, trace2])
 
-app.layout = dbc.Container(
-    [
-        html.H1("Iris k-means clustering"),
-        html.Hr(),
-        dbc.Row(
-            [
-                dbc.Col(controls, md=4),
-                dbc.Col(dcc.Graph(id="cluster-graph"), md=8),
-            ],
-            align="center",
-        ),
-    ],
-    fluid=True,
-)
+# initialize xaxis2 and yaxis2
+fig['layout']['xaxis2'] = {}
+fig['layout']['yaxis2'] = {}
 
+# Edit layout for subplots
+fig.layout.yaxis.update({'domain': [0, .45]})
+fig.layout.yaxis2.update({'domain': [.6, 1]})
 
-@app.callback(
-    Output("cluster-graph", "figure"),
-    [
-        Input("x-variable", "value"),
-        Input("y-variable", "value"),
-        Input("cluster-count", "value"),
-    ],
-)
-def make_graph(x, y, n_clusters):
-    # minimal input validation, make sure there's at least one cluster
-    km = KMeans(n_clusters=max(n_clusters, 1))
-    df = iris.loc[:, [x, y]]
-    km.fit(df.values)
-    df["cluster"] = km.labels_
+# The graph's yaxis2 MUST BE anchored to the graph's xaxis2 and vice versa
+fig.layout.yaxis2.update({'anchor': 'x2'})
+fig.layout.xaxis2.update({'anchor': 'y2'})
+fig.layout.yaxis2.update({'title': 'Goals'})
 
-    centers = km.cluster_centers_
+# Update the margins to add a title and see graph x-labels.
+fig.layout.margin.update({'t':75, 'l':50})
+fig.layout.update({'title': '2016 Hockey Stats'})
 
-    data = [
-        go.Scatter(
-            x=df.loc[df.cluster == c, x],
-            y=df.loc[df.cluster == c, y],
-            mode="markers",
-            marker={"size": 8},
-            name="Cluster {}".format(c),
-        )
-        for c in range(n_clusters)
-    ]
+# Update the height because adding a graph vertically will interact with
+# the plot height calculated for the table
+fig.layout.update({'height':800})
 
-    data.append(
-        go.Scatter(
-            x=centers[:, 0],
-            y=centers[:, 1],
-            mode="markers",
-            marker={"color": "#000", "size": 12, "symbol": "diamond"},
-            name="Cluster centers",
-        )
-    )
-
-    layout = {"xaxis": {"title": x}, "yaxis": {"title": y}}
-
-    return go.Figure(data=data, layout=layout)
-
-
-# make sure that x and y values can't be the same variable
-def filter_options(v):
-    """Disable option v"""
-    return [
-        {"label": col, "value": col, "disabled": col == v}
-        for col in iris.columns
-    ]
-
-
-# functionality is the same for both dropdowns, so we reuse filter_options
-app.callback(Output("x-variable", "options"), [Input("y-variable", "value")])(
-    filter_options
-)
-app.callback(Output("y-variable", "options"), [Input("x-variable", "value")])(
-    filter_options
-)
+# Plot!
+fig.show()
 
 
 if __name__ == "__main__":
